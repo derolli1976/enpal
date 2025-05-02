45417e2435fc63401e4c20d85d7c6cd5ab4f4335
Oliver Stock <stock.o@hotmail.de>
2025-05-02 18:43:22 +0200

    bugfix config

--- custom_components/enpal_webparser/config_flow.py
+++ custom_components/enpal_webparser/config_flow.py
@@ -1,19 +1,23 @@
-from homeassistant import config_entries
+import logging
 import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.helpers import config_validation as cv
 
 from .const import DOMAIN, DEFAULT_URL, DEFAULT_INTERVAL, DEFAULT_GROUPS
 
+_LOGGER = logging.getLogger(__name__)
+
 GROUP_OPTIONS = ["Wallbox", "Battery", "Inverter", "Site Data", "IoTEdgeDevice", "PowerSensor"]
 
-class EnpalConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class EnpalParserFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        errors = {}
+        _LOGGER.info("Enpal Parser: config flow gestartet")
 
         if user_input is not None:
             return self.async_create_entry(
-                title="Enpal Webparser",
+                title="Enpal Parser",
                 data={
                     "url": user_input.get("url", DEFAULT_URL),
                     "interval": user_input.get("interval", DEFAULT_INTERVAL),
@@ -24,10 +28,7 @@ class EnpalConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         schema = vol.Schema({
             vol.Required("url", default=DEFAULT_URL): str,
             vol.Required("interval", default=DEFAULT_INTERVAL): int,
-            vol.Optional("groups", default=DEFAULT_GROUPS): vol.All(
-                vol.EnsureList,
-                vol.In(GROUP_OPTIONS),
-            )
+            vol.Optional("groups", default=DEFAULT_GROUPS): cv.multi_select(GROUP_OPTIONS)
         })
 
-        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+        return self.async_show_form(step_id="user", data_schema=schema, errors={})
