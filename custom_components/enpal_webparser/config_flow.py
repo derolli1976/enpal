79ada93467b295be2630972a2b76a85d2f5bc9f0
Oliver Stock <stock.o@hotmail.de>
2025-05-02 17:59:53 +0200

    config flow fix

--- custom_components/enpal_webparser/config_flow.py
+++ custom_components/enpal_webparser/config_flow.py
@@ -3,6 +3,8 @@ import voluptuous as vol
 
 from .const import DOMAIN, DEFAULT_URL, DEFAULT_INTERVAL, DEFAULT_GROUPS
 
+GROUP_OPTIONS = ["Wallbox", "Battery", "Inverter", "Site Data", "IoTEdgeDevice", "PowerSensor"]
+
 class EnpalConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
@@ -13,18 +15,18 @@ class EnpalConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(
                 title="Enpal Webparser",
                 data={
-                    "url": user_input["url"],
-                    "interval": user_input["interval"],
-                    "groups": user_input["groups"],
+                    "url": user_input.get("url", DEFAULT_URL),
+                    "interval": user_input.get("interval", DEFAULT_INTERVAL),
+                    "groups": user_input.get("groups", []),
                 },
             )
 
         schema = vol.Schema({
             vol.Required("url", default=DEFAULT_URL): str,
             vol.Required("interval", default=DEFAULT_INTERVAL): int,
-            vol.Required("groups", default=DEFAULT_GROUPS): vol.All(
+            vol.Optional("groups", default=DEFAULT_GROUPS): vol.All(
                 vol.EnsureList,
-                vol.In(["Wallbox", "Battery", "Inverter", "Site Data", "IoTEdgeDevice", "PowerSensor"]),
+                vol.In(GROUP_OPTIONS),
             )
         })
 
