12bf8159f7ba0bfdb5e7316212a42e11b5981e58
Oliver Stock <stock.o@hotmail.de>
2025-10-14 17:05:32 +0200

    Change battery charge load type to energy_storage (#74)

--- custom_components/enpal_webparser/const.py
+++ custom_components/enpal_webparser/const.py
@@ -62,7 +62,7 @@ DEVICE_CLASS_OVERRIDES = {
     "energy_battery_charge_level_unit_1": "battery",
     "energy_battery_charge_level_unit_2": "battery",
     "energy_battery_charge_level_absolute": "battery",
-    "energy_battery_charge_load": "battery",
+    "energy_battery_charge_load": "energy_storage",
 }
 
 STATE_CLASS_OVERRIDES = {
